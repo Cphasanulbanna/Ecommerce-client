5ab3ca19e61f693f3818de96f675f25096d1a7fe
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-12 17:23:07 +0530

    password toggling added

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 const Login = () => {
     const login = async (e) => {
         e.preventDefault();
     };
+
+    const [viewPassword, setViewPassword] = useState(false);
     return (
         <div className="w-full h-full sm:h-max sm:max-w-md sm:mx-auto rounded-none justify-center gap-4 flex flex-col sm:gap-8 bg-white p-6 sm:p-12 sm:rounded-lg shadow-sm font-semibold">
             <h1 className="auth-heading">Login to your account</h1>
@@ -32,11 +35,24 @@ const Login = () => {
                     >
                         Password
                     </label>
-                    <input
-                        type="text"
-                        placeholder="Enter password"
-                        className="auth-input"
-                    />
+                    <div className="relative">
+                        <input
+                            type={viewPassword ? "text" : "password"}
+                            placeholder="Enter password"
+                            className="auth-input"
+                        />
+                        <div className="absolute right-2 top-[50%] translate-y-[-50%] h-full flex justify-center items-center">
+                            {viewPassword ? (
+                                <button onClick={() => setViewPassword((prev) => !prev)}>
+                                    <AiFillEye size={20} />
+                                </button>
+                            ) : (
+                                <button onClick={() => setViewPassword((prev) => !prev)}>
+                                    <AiFillEyeInvisible size={20} />
+                                </button>
+                            )}
+                        </div>
+                    </div>
                 </div>
                 <button
                     type="submmit"
