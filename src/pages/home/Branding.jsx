81e990f40a0425cb1696eb11effcce55262c3543
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-20 20:46:15 +0530

    branding section styling completed

--- src/pages/home/Branding.jsx
+++ src/pages/home/Branding.jsx
@@ -6,7 +6,7 @@ import { brandingData } from "../../static/data";
 
 const Branding = () => {
     return (
-        <div className="sm:py-[50px] py-[40px] bg-violet-100">
+        <div className="section-style">
             <motion.div
                 initial="hidden"
                 whileInView="visible"
@@ -17,12 +17,12 @@ const Branding = () => {
                     visible: { opacity: 1, y: 0 },
                 }}
             >
-                <div className="flex items-center justify-between p-6 bg-white rounded-md shadow-md wrapper">
+                <div className="rounded-md md:shadow-md md:grid md:p-6 md:bg-white md:gap-8 xl:gap-5 sm:grid-cols-2 gap-9 xl:grid-cols-4 wrapper">
                     {brandingData &&
                         brandingData?.map((data) => (
                             <div
                                 key={data.id}
-                                className="flex items-center gap-4"
+                                className="flex items-center gap-4 p-4 mb-4 bg-white rounded-md shadow-sm md:shadow-none md:p-0"
                             >
                                 <div className="mb-3">{data.icon}</div>
                                 <div className="flex flex-col gap-1">
