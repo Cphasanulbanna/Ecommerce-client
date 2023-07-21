dfcadec9acf50216c4d877bbce19131b7b9a67b0
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-21 16:12:30 +0530

    event section responsive completed

--- src/pages/home/Events.jsx
+++ src/pages/home/Events.jsx
@@ -5,8 +5,8 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 const Events = ({ inPage }) => {
     return (
         <section className={`${inPage ? "py-0" : "py-8 lg:py-16"}  bg-violet-100`}>
-            <div className="flex flex-col items-center justify-between p-12 overflow-hidden bg-white rounded-md shadow-md sm:flex-row wrapper">
-                <div className="sm:w-[40%] md:w-[50%] w-[50%]">
+            <div className="flex flex-col items-center justify-between p-6 overflow-hidden bg-white rounded-md shadow-md sm:p-8 md:p-12 sm:flex-row wrapper">
+                <div className="sm:w-[40%] md:w-[45%] lg:w-[40%] xl:w-[35%] w-[50%]">
                     <img
                         src="https://images.tcdn.com.br/img/img_prod/893885/apple_iphone_14_128gb_roxo_6_1_12mp_ios_5g_com_carregador_25w_usb_c_power_adapter_usb_c_to_lightning_4147_1_c12ded022c2bbef33929495ce0bcec61.jpg"
                         alt="product"
@@ -15,7 +15,7 @@ const Events = ({ inPage }) => {
                 </div>
                 <div className="flex flex-col md:w-[50%] sm:w-[60%] sm:gap-3 gap-2 w-full">
                     <h2 className="text-2xl font-bold sm:text-3xl">product name</h2>
-                    <p className="hidden line-clamp-6 md:block">
+                    <p className="hidden lg:line-clamp-4 xl:line-clamp-6">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ex quia
                         facilis repellendus expedita eum natus rem placeat animi explicabo, ratione
                         illo cum, adipisci recusandae. Ea corporis, ipsam natus quaerat vitae
