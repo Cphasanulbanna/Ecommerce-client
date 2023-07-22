f7b450f76b505981604a1ea60d4de4f9693aca30
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-22 22:47:22 +0530

    product details page responsive completed

--- src/components/general/BottomNavigation.jsx
+++ src/components/general/BottomNavigation.jsx
@@ -9,7 +9,7 @@ import { TbCategory } from "react-icons/tb";
 
 const BottomNavigation = () => {
     return (
-        <nav className="fixed bottom-0 left-0 right-0 z-50 block px-4 py-2 bg-white sm:hidden">
+        <nav className="fixed bottom-0 left-0 right-0 z-40 block px-4 py-2 bg-white sm:hidden">
             <div className="flex justify-between gap-4">
                 <NavLink
                     className={({ isActive }) => (isActive ? "text-yellow" : "")}
--- src/pages/product/ProductsDetailsPage.jsx
+++ src/pages/product/ProductsDetailsPage.jsx
@@ -6,6 +6,7 @@ import { AiOutlineMessage } from "react-icons/ai";
 import { FaCartPlus } from "react-icons/fa";
 import { MdOutlineAdd } from "react-icons/md";
 import { MdRemove } from "react-icons/md";
+import { IoMdFlash } from "react-icons/io";
 
 const ProductsDetailsPage = () => {
     useEffect(() => {
@@ -13,59 +14,61 @@ const ProductsDetailsPage = () => {
     }, []);
     const product = productData[0];
     return (
-        <section className="py-16 bg-slate-50">
-            <div className="wrapper-secondary">
-                <div className="grid grid-cols-2 gap-5 p-10 bg-white rounded-md shadow-md">
-                    <div className="flex flex-col gap-3">
-                        <div>
-                            <img
-                                src={product.image_Url[0].url}
-                                alt="product"
-                                className="object-cover"
-                            />
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <div className="w-[60px] h-[60px] rounded-full overflow-hidden">
+        <section className="py-16 sm:bg-slate-50">
+            <div className="sm:wrapper">
+                <div className="flex flex-col justify-between gap-5 p-4 bg-white rounded-md sm:shadow-md sm:p-10 md:flex-row">
+                    <div className="flex flex-col gap-3 w-full md:w-[45%] xl:w-[35%]">
+                        <div className="p-3 border-slate-100 border-solid border-[1px]">
+                            <div>
                                 <img
-                                    src={product.shop.shop_avatar}
+                                    src={product.image_Url[0].url}
+                                    alt="product"
+                                    className="object-cover"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-between sm:gap-2 md:flex-col lg:flex-row sm:static">
+                            <button className="text-white bg-[#fb641b] btn min-h-[40px] h-[40px] w-1/2 border-none rounded-none md:w-full lg:w-[48%] sm:w-[48%]">
+                                <span>Add to cart</span>
+                                <FaCartPlus size={16} />
+                            </button>
+                            <button className="text-white bg-[#ff9f0e] btn min-h-[40px] h-[40px] w-1/2 border-none rounded-none md:w-full lg:w-[48%] sm:w-[48%]">
+                                <span>Buy now</span>
+                                <IoMdFlash size={16} />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-5 w-full md:w-[55%] xl:w-[65%]">
+                        <h1 className="font-bold sm:text-2xl">{product.name}</h1>
+                        <p className="text-sm">{product.description}</p>
+                        <div className="border-slate-100 border-solid border-[1px] p-3 flex flex-col gap-2 sm:gap-3">
+                            <div className="flex items-center gap-2">
+                                <h5>{product.price}$</h5>
+                                <small className="line-through sm:text-xs text-red">
+                                    {product.discount_price}$
+                                </small>
+                            </div>
+
+                            <span>Ratings ({product.rating})</span>
+                            <span>Sold {product.total_sell}</span>
+                            <span>Stock {product.stock}</span>
+                        </div>
+
+                        <button className="text-white btn w-max bg-sky-500 min-h-[40px] h-[40px] rounded-none">
+                            <span>Send Message</span>
+                            <AiOutlineMessage size={22} />
+                        </button>
+
+                        <div className="flex items-center gap-2 border-slate-100 border-solid border-[1px] w-max p-3">
+                            <div className="w-[30px] h-[30px] rounded-full overflow-hidden">
+                                <img
+                                    src={product.shop.shop_avatar.url}
                                     alt="brand-image"
                                 />
                             </div>
                             <p className="text-sky-500">{product.shop.name}</p>
                         </div>
-                        <span>Ratings ({product.rating})</span>
-                        <span>Sold {product.total_sell}</span>
-                        <span>Stock {product.stock}</span>
-                        <button className="text-white btn w-max bg-sky-500">
-                            <span>Send Message</span>
-                            <AiOutlineMessage size={22} />
-                        </button>
-                    </div>
-                    <div className="flex flex-col gap-5">
-                        <h1 className="text-2xl font-bold">{product.name}</h1>
-                        <p className="">{product.description}</p>
-                        <div className="flex items-center gap-2">
-                            <h5>{product.price}$</h5>
-                            <small className="text-xs line-through text-red">
-                                {product.discount_price}$
-                            </small>
-                        </div>
-
-                        <div className="flex items-center gap-4">
-                            <button className="text-white bg-green-500 btn btn-square">
-                                <MdOutlineAdd size={20} />
-                            </button>
-                            <span className="w-[48px] h-[48px] rounded-md flex justify-center items-center text-white bg-green-500 font-bold">
-                                {1}
-                            </span>
-                            <button className="text-white bg-green-500 btn btn-square">
-                                <MdRemove size={20} />
-                            </button>
-                        </div>
-                        <button className="text-white bg-green-500 btn w-max">
-                            <span>Add to cart</span>
-                            <FaCartPlus size={25} />
-                        </button>
                     </div>
                 </div>
             </div>
