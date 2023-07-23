796e53a77c4677a62237416e3b606d56e3795b5b
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-23 15:27:38 +0530

    image selection added in product-details page

--- src/pages/product/ProductCard.jsx
+++ src/pages/product/ProductCard.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import StarRatings from "react-star-ratings";
 
 const ProductCard = ({ product, inHome }) => {
+    // const productName = product?.name.replaceAll("-", " ");
     return (
         <div
             id="best-selling-product"
--- src/pages/product/ProductsDetailsPage.jsx
+++ src/pages/product/ProductsDetailsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { productData } from "../../static/data";
 
@@ -7,12 +7,28 @@ import { FaCartPlus } from "react-icons/fa";
 import { IoMdFlash } from "react-icons/io";
 
 import { motion } from "framer-motion";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ProductsDetailsPage = () => {
+    const [product, setProduct] = useState(null);
+    const { id } = useParams();
+
+    console.log(id, "id");
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: "instant" });
-    }, []);
-    const product = productData[0];
+
+        const product = productData?.find((product) => product.id == id);
+        setProduct(product);
+    }, [id]);
+
+    const [selectedImage, setSelectedImage] = useState(0);
+
+    const navigate = useNavigate();
+
+    const sendMessage = () => {
+        navigate("/inbox?conversation=505se5d1ee44d4");
+    };
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -23,11 +39,30 @@ const ProductsDetailsPage = () => {
         >
             <div className="sm:wrapper">
                 <div className="flex flex-col justify-between gap-5 p-4 bg-white rounded-md sm:shadow-md sm:p-10 md:flex-row">
+                    <div className="w-[5%] min-w-[60px] flex flex-col h-full py-1 overflow-x-hidden overflow-y-auto">
+                        {product?.image_Url?.map((image, index) => (
+                            <div
+                                key={index}
+                                onClick={() => setSelectedImage(index)}
+                                className={`${
+                                    selectedImage === index
+                                        ? " border-solid border-violet-800 border-[2px]"
+                                        : ""
+                                } w-[60px] h-[60px] cursor-pointer border-solid border-slate-200 border-[1px]`}
+                            >
+                                <img
+                                    src={image.url}
+                                    alt="product-image"
+                                    className="object-contain"
+                                />
+                            </div>
+                        ))}
+                    </div>
                     <div className="flex flex-col gap-3 w-full md:w-[45%] xl:w-[35%]">
                         <div className="p-3 border-slate-100 border-solid border-[1px]">
                             <div>
                                 <img
-                                    src={product.image_Url[0].url}
+                                    src={product?.image_Url[selectedImage]?.url}
                                     alt="product"
                                     className="object-cover"
                                 />
@@ -45,20 +80,20 @@ const ProductsDetailsPage = () => {
                             </button>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-5 w-full md:w-[55%] xl:w-[65%]">
-                        <h1 className="font-bold sm:text-2xl">{product.name}</h1>
-                        <p className="text-sm">{product.description}</p>
+                    <div className="flex flex-col gap-5 w-full md:w-[55%] xl:w-[60%]">
+                        <h1 className="font-bold sm:text-2xl">{product?.name}</h1>
+                        <p className="text-sm">{product?.description}</p>
                         <div className="border-slate-100 border-solid border-[1px] p-3 flex flex-col gap-2 sm:gap-3">
                             <div className="flex items-center gap-2">
-                                <h5>{product.price}$</h5>
+                                <h5>{product?.price}$</h5>
                                 <small className="line-through sm:text-xs text-red">
-                                    {product.discount_price}$
+                                    {product?.discount_price}$
                                 </small>
                             </div>
 
-                            <span>Ratings ({product.rating})</span>
-                            <span>Sold {product.total_sell}</span>
-                            <span>Stock {product.stock}</span>
+                            <span>Ratings ({product?.rating})</span>
+                            <span>Sold {product?.total_sell}</span>
+                            <span>Stock {product?.stock}</span>
                         </div>
 
                         <button className="text-white btn w-max bg-sky-500 min-h-[40px] h-[40px] rounded-none">
@@ -69,11 +104,11 @@ const ProductsDetailsPage = () => {
                         <div className="flex items-center gap-2 border-slate-100 border-solid border-[1px] w-max p-3">
                             <div className="w-[30px] h-[30px] rounded-full overflow-hidden">
                                 <img
-                                    src={product.shop.shop_avatar.url}
+                                    src={product?.shop?.shop_avatar.url}
                                     alt="brand-image"
                                 />
                             </div>
-                            <p className="text-sky-500">{product.shop.name}</p>
+                            <p className="text-sky-500">{product?.shop.name}</p>
                         </div>
                     </div>
                 </div>
