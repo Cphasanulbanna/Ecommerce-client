c4cd102f2b40ba9b8983a6975cc41133673a8274
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-24 17:20:03 +0530

    created order-tracking page

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import Address from "./Address";
 import Orders from "./Orders";
+import Refund from "./Refund";
+import TrackOrder from "./TrackOrder";
 
 const Profile = ({ active }) => {
     return (
         <div className="w-[fill] sm:px-8 px-3 pb-10 pt-24 sm:py-0 min-h-screen">
-            {active === 1 && <Address />} {active === 2 && <Orders />}
+            {active === 1 && <Address />} {active === 2 && <Orders />} {active === 4 && <Refund />}
+            {active === 3 && <TrackOrder />}
         </div>
     );
 };
